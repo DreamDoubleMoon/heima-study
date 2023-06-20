1386ce38ce7f0c1286d518bf2faf09aa69c6ad37
MoonLuo <104648587+DreamDoubleMoon@users.noreply.github.com>
2023-06-20 20:14:02 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int a = 16;
+	cout <<  sizeof(short int) << endl;
+	cout << sizeof(int) << endl;
+	cout <<  sizeof(long) << endl;
+	cout << sizeof(long long) << endl;
+	cout << sizeof(float) << endl;
+	cout << sizeof(double) << endl;
+	cout << a << endl;
+	cout << sizeof(char) << endl;
+	system("pause");
+
+	return 0;
+	
+}
